1ca89a63d3a529fa498df609502eeb1121ef701a
flo2201 <florian.licour@gmail.com>
2011-02-03 18:02:21 +0100

    add operator

--- UInteger.cpp
+++ UInteger.cpp
@@ -134,27 +134,30 @@ UInteger UInteger::operator +(const UInteger &x)const{
     }
     UInteger *result = new UInteger();
     int retenue =0;
-
-    //std::list<char>::iterator itr = (*result).digits_.begin();
     std::_List_const_iterator<char> ita = uia.digits_.begin();
     for (std::_List_const_iterator<char> itb = uib.digits_.begin();itb != uib.digits_.end() || ita!= uia.digits_.end();ita++) {
         (*result).digits_.push_back((retenue+*itb+*ita)%base_);
         retenue=(retenue+*ita+*itb)/base_;
         itb++;
     }
-    /*if(ita != digits_.end()){
-        ita++;
-        (*result).digits_.push_back((retenue+*ita)%base_);
-        retenue=(retenue+*ita)/base_;
-    }*/
     return *result;
  }
 UInteger UInteger::operator -(const UInteger &x)const{
+//ajout de zero
+    UInteger uia,uib;
+    if((x.getDigits().size()) > getDigits().size()){
+        uia=bourage(x.getDigits().size()-getDigits().size());
+        uib=x;
+    }
+    else{
+        uib=x.bourage(getDigits().size()-x.getDigits().size());
+        uia=*this;
+    }
     int retenue =0;
     UInteger *result = new UInteger();
-    std::list<char>::iterator itr = (*result).digits_.begin();
-    std::_List_const_iterator<char> ita = x.digits_.begin();
-    for (std::_List_const_iterator<char> itb = digits_.begin();itb != digits_.end() || ita!= x.digits_.end();ita++) {
+// calcul
+    std::_List_const_iterator<char> itb = uia.digits_.begin();
+    for (std::_List_const_iterator<char> ita = uib.digits_.begin();ita != uib.digits_.end() || itb!= uia.digits_.end();ita++) {
         if((*itb-*ita-retenue)%base_<0){
             (*result).digits_.push_back( base_+(*itb-*ita-retenue));
             retenue=1;
@@ -168,16 +171,22 @@ UInteger UInteger::operator -(const UInteger &x)const{
     return *result;
  }
  UInteger& UInteger::operator -=(const UInteger &x){
-    UInteger *result;
+    //ajout de zero
+    UInteger uia,uib;
+    if((x.getDigits().size()) > getDigits().size()){
+        uia=bourage(x.getDigits().size()-getDigits().size());
+        uib=x;
+    }
+    else{
+        uib=x.bourage(getDigits().size()-x.getDigits().size());
+        uia=*this;
+    }
     int retenue =0;
-    std::_List_const_iterator<char> ita = x.digits_.begin();
-    for (std::_List_iterator<char> itb = digits_.begin();itb != digits_.end() || ita!= x.digits_.end();ita++) {
+    std::_List_iterator<char> itb = uia.digits_.begin();
+    for (std::_List_const_iterator<char> ita = uib.digits_.begin();ita != uib.digits_.end() || itb!= uia.digits_.end();ita++) {
         if((*itb-*ita-retenue)%base_<0){
-        //if(retenue >0 ){
             *itb = (int) base_+(*itb-*ita-retenue);
             retenue=1;
-        //}
-        //else *itb = (int) 0;
         }
         else {
           *itb=(*itb-*ita-retenue);
@@ -185,10 +194,7 @@ UInteger UInteger::operator -(const UInteger &x)const{
         }
             itb++;
     }
-    result=this;
-
-
-
+    digits_=uia.digits_;
   return *this;
  }
  UInteger& UInteger::operator +=(const UInteger &x){
